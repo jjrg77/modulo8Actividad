204bab1682169d0961da89b6007600782087bd12
jjrg77 <jjrgmadrid@gmail.com>
2021-05-02 20:50:26 +0200

    ejercicio finalizado - módulo 8

--- js/main.js
+++ js/main.js
@@ -1,87 +1,95 @@
-const url = 'http://api.icndb.com/jokes/random';
+const url = "http://api.icndb.com/jokes/random";
 
 // FUNCIÓN PARA MODIFICAR DOM Y VISUALIZAR VALOR DE LA PROPIEDAD JOKE
 const modifyDOM = (joke) => {
-    const element = document.getElementById('card')
-    const elementChild = document.createElement('div');
-    elementChild.innerHTML = `
+  const element = document.getElementById("card");
+  const elementChild = document.createElement("div");
+  elementChild.innerHTML = `
     <div class="card">
         <p>${joke}</p>
     </div>
     `;
-    element.appendChild(elementChild);
-    setTimeout(() => {
-        document.querySelector('.card').remove();
-    }, 2000);
+  element.appendChild(elementChild);
+  setTimeout(() => {
+    document.querySelector(".card").remove();
+  }, 2000);
 };
 
 // FUNCIÓN MÉTODO XMLHTTPREQUEST
 const functionHttp = () => {
-    //1- instanciamos
-    const xhr = new XMLHttpRequest();
-        // $xhr = document.getElementById('xhr');
+  //1- instanciamos
+  const xhr = new XMLHttpRequest();
+  //2- asignación de evento(s)
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
 
-    //2- asignación de evento(s)
-    xhr.addEventListener('readystatechange', (e) => {
-        if (xhr.readyState !== 4) return; 
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let data = JSON.parse(xhr.response);
+      const joke = data.value.joke;
+      modifyDOM(joke);
+    } else {
+      let message = xhr.statusText || "Ocurrió un error";
+      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+    }
+  });
 
-        if (xhr.status >= 200 && xhr.status < 300) {
-            console.log('éxito');
-            console.log(xhr.response);
-            let json = JSON.parse(xhr.response);
-            const joke = json.value.joke;
+  //3- Abrimos petición
+  xhr.open("GET", url);
 
-            modifyDOM(joke);
-
-        } else {
-            let message = xhr.statusText || 'Ocurrió un error';
-            $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
-        }
-    });
-
-    //3- Abrimos petición
-    xhr.open('GET', url);
-
-    //4- enviamos petición
-    xhr.send();
+  //4- enviamos petición
+  xhr.send();
 };
 
 // FUNCIÓN MÉTODO FETCH
 const functionFetch = () => {
-
-    fetch(url)
-    .then(res => {
-        console.log(res);
-        // RES es del tipo 'ReadableStream' por lo que lo pasaremos a JSON
-        const resJson = res.json();
-        console.log(resJson);
-        //devolvemos RES al siguiente 'then'
+  fetch(url)
+    //en esta promesa le indicamos que nos pase los datos a JSON
+    .then((response) => response.json())
+    //DATA es el resultado de response.json. Creamos constante y extraemos el dato que queremos
+    .then((data) => {
+      const joke = data.value.joke;
+      modifyDOM(joke);
     })
-    .then(resJson => {
-        console.log(json);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    //en caso de error, lo sacamos por consola
+    .catch((err) => console.log(err));
 };
 
+// FUNCIÓN MÉTODO FETCH + ASYNC - AWAY (FUNCIÓN ASÍNCRONA)
+async function functionFetchAsync() {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const joke = data.value.joke;
+    modifyDOM(joke);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+const functionAxios = () => {
+  axios({
+    method: "GET",
+    url: url,
+  })
+    .then((res) => {
+      const joke = res.data.value.joke;
+      modifyDOM(joke);
+    })
+    .catch((err) => console.log(err));
+};
 
-
-document.getElementById('buttonId1').addEventListener('click', () => {
-    functionHttp();
+document.getElementById("buttonId1").addEventListener("click", () => {
+  functionHttp();
 });
 
-document.getElementById('buttonId2').addEventListener('click', () => {
-    functionFetch();
+document.getElementById("buttonId2").addEventListener("click", () => {
+  functionFetch();
 });
 
-document.getElementById('buttonId3').addEventListener('click', () => {
-    // functionFetchAsync();
+document.getElementById("buttonId3").addEventListener("click", () => {
+  functionFetchAsync();
 });
 
-document.getElementById('buttonId4').addEventListener('click', () => {
-    // functionFetchAxios();
+document.getElementById("buttonId4").addEventListener("click", () => {
+  functionAxios();
 });
-    
-
